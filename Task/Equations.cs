ac3ae64ea226688bec9162e5ee1b6861dfef235d
Sergei Prutsakov <psn206@mail.ru>
2024-04-27 20:03:17 +0300

    Добавьте файлы проекта.

---
+++ Task/Equations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal struct Equations
+    {
+        double coeffK;
+        double coeffB;
+
+        public Equations(double coeffK, double coeffB)
+        {
+            this.coeffK = coeffK;
+            this.coeffB = coeffB;
+        }
+
+        public double Root()
+        {
+            return   -coeffB/coeffK;
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = double.MinValue;
+            Console.WriteLine("Введите коэфф. k и b для решения линейного уравнения:");
+            do
+            {
+                Console.Write("k= ");
+                k = double.Parse(Console.ReadLine());
+                if (k == 0)
+                {
+                    Console.WriteLine("k не может быть равно 0!. Повтооите ввод.");
+                }
+            }
+            while (k == 0);
+            Console.Write("b= ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{k}x{b:+0;-0;0}=0");
+            Equations equations = new Equations(k, b);
+            Console.WriteLine($"x={equations.Root():f2}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
